ad1f128ecd53d85eb218f45b2444301cdb3df1e5
Varsha Tumburu <54908599+varshatumburu@users.noreply.github.com>
2021-05-21 11:02:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,25 @@ import os
 import cv2
 import skimage.exposure
 
+# All the plant species in the trained dataset
 labels = ["Ulmus carpinifolia", "Acer", "Salix aurita", "Quercus", "Alnus incana", "Betula pubescens", "Salix alba 'Sericea",
 	"Populus tremula", "Ulmus glabra", "Sorbus aucuparia", "Salix sinerea", "Populus", "Tilia", "Sorbus intermedia", "Fagus silvatica"]
 	
+# Function to predict the species of the plant based on the trained model and input image
 def prediction(img_path, model):
+	# Image pre-processing 
 	img = image.load_img(img_path,target_size=(224,224),grayscale=True)
 	img = image.img_to_array(img)
 	img = img.reshape(-1, 224, 224, 1)
 	img = img.astype('float32')
 	img = img/255.0
-    
+    	# ---
 	lst = model.predict(img)
 	index = np.argmax(lst,axis=-1)[0]
 	return index, lst[0][index]
 
+# Applying a mask to images with a noisy background image helps in more accurate classification
+# This mask filters out all the green elements from the image, keeping in mind leaves are usually green
 def apply_mask(img_path):
 	img = cv2.imread(img_path)
 	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -36,6 +41,7 @@ def apply_mask(img_path):
 	cv2.imwrite("result.jpg", result)
 	return result
 
+# Focuses mainly on the UI designs from here and calling the requred functions when necessary 
 def main():
 	st.write("""
 		## Swedish Leaf Dataset Image Classifier 
@@ -65,6 +71,12 @@ def main():
 					st.success("This leaf is from the species: {} => Accuracy: {}%".format(labels[result],round(acc*100,2)))
 				else:
 					st.success("This leaf is from the species: {} => Accuracy: {}%".format(labels[result],round(acc*100,2)))
+				# Adds a google search link to the predicted species for a better user experience 
+				search_fields = labels[result].split()
+				search = ""
+				for tag in search_fields:
+					search+=tag+"+"
+				st.write("[Click here to learn more about these species!](https://www.google.com/search?q={}leaf)".format(search))
 		else:
 			st.write(""" Please upload an image. """)
 
